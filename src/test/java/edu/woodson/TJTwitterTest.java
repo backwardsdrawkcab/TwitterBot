67dbec380c8e6e7620ecce77059f761518a7085a
SirMathhman <sirmathhman@gmail.com>
2019-02-26 14:49:16 -0500

    Add TJTwitter

---
+++ src/test/java/edu/woodson/TJTwitterTest.java
@@ -0,0 +1,48 @@
+package edu.woodson;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TJTwitterTest {
+
+    @Test
+    void getTerms() {
+    }
+
+    @Test
+    void getNumberOfTweets() {
+    }
+
+    @Test
+    void getMostPopularWord() {
+    }
+
+    @Test
+    void getFrequencyMax() {
+    }
+
+    @Test
+    void tweetOut() {
+    }
+
+    @Test
+    void queryHandle() {
+    }
+
+    @Test
+    void toMessage() {
+    }
+
+    @Test
+    void removePunctuation() {
+    }
+
+    @Test
+    void investigate() {
+    }
+
+    @Test
+    void sampleInvestigate() {
+    }
+}
