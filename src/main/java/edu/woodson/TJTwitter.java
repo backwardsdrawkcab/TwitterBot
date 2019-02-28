f8e0a8f2f2478fa6fdd739f0a217342854531e7b
SirMathhman <sirmathhman@gmail.com>
2019-02-28 13:53:28 -0500

    Implement TJTwitter.tweetOut()

--- src/main/java/edu/woodson/TJTwitter.java
+++ src/main/java/edu/woodson/TJTwitter.java
@@ -19,7 +19,7 @@ class TJTwitter {
     private List<String> terms;
     private String popularWord;
     private int frequencyMax;
-  
+
     public TJTwitter() {
         // Makes an instance of Twitter - this is re-usable and thread safe.
         // Connects to Twitter and performs authorizations.
@@ -51,7 +51,7 @@ class TJTwitter {
      * @param message a message you wish to Tweet out
      */
     public void tweetOut(String message) throws TwitterException, IOException {
-
+        twitter.updateStatus(message);
     }
 
 
