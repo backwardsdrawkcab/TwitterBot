d21c1cfd7cab1f9a3713d6c8b90b862d48c00c8a
Josh H <hilbertjosh1@gmail.com>
2019-03-08 15:11:19 -0500

    Found workaround for broken getCards method

--- src/test/java/edu/woodson/TrelloTest.java
+++ src/test/java/edu/woodson/TrelloTest.java
@@ -2,11 +2,13 @@ package edu.woodson;
 
 import com.julienvey.trello.Trello;
 import com.julienvey.trello.domain.Board;
+import com.julienvey.trello.domain.Card;
 import com.julienvey.trello.domain.TList;
 import com.julienvey.trello.impl.TrelloImpl;
 import com.julienvey.trello.impl.http.ApacheHttpClient;
 
 import java.util.List;
+import java.util.Scanner;
 
 public class TrelloTest {
 
@@ -20,11 +22,63 @@ public class TrelloTest {
     private static final String TOKEN = "186d9044cd5dfdb60c3b5ab3befb2aaeb2daddccdd05244ce152743701fec680";
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
         Trello trello = new TrelloImpl(API_KEY, TOKEN, new ApacheHttpClient());
-        Board board = trello.getBoard("dqJhYMUB");
-        List<TList> lists = board.fetchLists();
-        for (TList tl:lists) {
-            System.out.println(tl.getName());
+        String boardId = "dqJhYMUB";
+        Board board = trello.getBoard(boardId);
+
+//        for (Card c: board.fetchCards()) {
+//            System.out.println(c.getName());
+//        }
+
+
+        List<TList> currentLists = board.fetchLists();
+        TList list = currentLists.get(0);
+        List<Card> cards = trello.getListCards(list.getId());
+        for (Card c:cards) {
+            System.out.println(c.getName()); //YESSSS!!!!!!
         }
+
+//        for (int i = 0; i < list.getCards().size(); i++) {
+//
+//        }
+
+//        for (TList tl:currentLists) {
+//            for (Card c:tl.getCards()) {
+//                System.out.println(tl.toString());
+//            }
+//        }
+
+//        String nothing = sc.next();
+//        List<TList> newLists = board.fetchLists();
+//
+//        System.out.println(findChangedCard(currentLists, newLists));
+    }
+
+    public static Card findChangedCard(List<TList> currentLists, List<TList> newLists) {
+        List<Card> current = null;
+        List<Card> newList = null;
+
+        int i = 0;
+
+        for (; i < newLists.size(); i++) {
+            if (!currentLists.get(i).getCards().equals(newLists.get(i).getCards())) {
+                current = currentLists.get(i).getCards();
+                newList = newLists.get(i).getCards();
+                continue;
+            }
+        }
+
+            if (i == newLists.size())
+                return null;
+
+                for (int j = 0; j < current.size(); j++) {
+                    if (!current.get(i).equals(newList.get(i))) {
+                        return newList.get(i);
+                    }
+                }
+
+        return null;
     }
 }
