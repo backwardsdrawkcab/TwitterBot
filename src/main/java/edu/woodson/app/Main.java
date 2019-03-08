b9a10518a4d2ed325685d358b82a158e4eaa7c66
SirMathhman <sirmathhman@gmail.com>
2019-03-08 13:54:26 -0500

    Add app methods, TrelloState

--- src/main/java/edu/woodson/app/Main.java
+++ src/main/java/edu/woodson/app/Main.java
@@ -2,6 +2,31 @@ package edu.woodson.app;
 
 public class Main {
     public static void main(String[] args) {
+        Main main = new Main();
+        main.start();
+
+        while(main.isRunning()){
+            main.loop();
+        }
+
+        main.stop();
+
         System.exit(0);
+    }
+
+    public void start(){
+
+    }
+
+    public boolean isRunning(){
+        return false;
+    }
+
+    public void loop(){
+
+    }
+
+    public void stop(){
+
     }
 }
