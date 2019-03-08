168761e1e85cd5f20d88155ef81f7fc1ae2cccb6
SirMathhman <sirmathhman@gmail.com>
2019-03-08 13:56:44 -0500

    Add TrelloState to TwitterBot

--- src/main/java/edu/woodson/app/TwitterBot.java
+++ src/main/java/edu/woodson/app/TwitterBot.java
@@ -1,6 +1,10 @@
 package edu.woodson.app;
 
+import edu.woodson.core.TrelloState;
+
 public class TwitterBot {
+    private final TrelloState state = new TrelloState();
+
     public TwitterBot() {
     }
 
