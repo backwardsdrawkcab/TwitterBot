a77a84940c70042f08d51ef2808e6cb9c2db8ec8
SirMathhman <minecraftmathhman@gmail.com>
2019-03-13 17:30:39 -0400

    Add test

---
+++ src/test/java/edu/woodson/lab/TJTwitterStatisticsTest.java
@@ -0,0 +1,57 @@
+package edu.woodson.lab;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author SirMathhman
+ * @version 0.0.0
+ * @since 3/13/2019
+ */
+class TJTwitterStatisticsTest {
+
+    @Test
+    void getMaxFrequency() {
+    }
+
+    @Test
+    void calculateMax() {
+    }
+
+    @Test
+    void createFrequencyMap() {
+    }
+
+    @Test
+    void getMostPopularWord() {
+    }
+
+    @Test
+    void mostPopularWord() {
+    }
+
+    @Test
+    void getStatuses() {
+    }
+
+    @Test
+    void getWords() {
+    }
+
+    @Test
+    void removeCommonWords() {
+    }
+
+    @Test
+    void setValues() {
+    }
+
+    @Test
+    void sortAndRemoveEntries() {
+    }
+
+    @Test
+    void removeEmptyStrings() {
+    }
+}
