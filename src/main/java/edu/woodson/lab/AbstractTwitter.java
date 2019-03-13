a17c5044bdf4ffb042cb33cc866547800b4117ae
SirMathhman <minecraftmathhman@gmail.com>
2019-03-13 17:57:29 -0400

    Add AbstractTwitter
    - This class will be empty and exists for testing purposes.

---
+++ src/main/java/edu/woodson/lab/AbstractTwitter.java
@@ -0,0 +1,11 @@
+package edu.woodson.lab;
+
+import twitter4j.Twitter;
+
+/**
+ * @author SirMathhman
+ * @version 0.0.0
+ * @since 3/13/2019
+ */
+public abstract class AbstractTwitter implements Twitter {
+}
