ee7167b352f1b840aeab5f440ac67295aa9ba18f
SirMathhman <minecraftmathhman@gmail.com>
2019-03-13 18:18:34 -0400

    Add AbstractStatus

---
+++ src/main/java/edu/woodson/lab/AbstractStatus.java
@@ -0,0 +1,11 @@
+package edu.woodson.lab;
+
+import twitter4j.Status;
+
+/**
+ * @author SirMathhman
+ * @version 0.0.0
+ * @since 3/13/2019
+ */
+public abstract class AbstractStatus implements Status {
+}
