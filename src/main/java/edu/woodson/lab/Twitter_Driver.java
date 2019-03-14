1072a030f7542c6676616cfcac3aff30a68c727c
JoshH <hilbertjosh1@gmail.com>
2019-03-13 23:10:08 -0400

    Add TrelloListener to Lab Driver

--- src/main/java/edu/woodson/lab/Twitter_Driver.java
+++ src/main/java/edu/woodson/lab/Twitter_Driver.java
@@ -1,14 +1,26 @@
 package edu.woodson.lab;// Name:
 // Date:
 
+import com.julienvey.trello.Trello;
+import com.julienvey.trello.domain.Board;
+import com.julienvey.trello.domain.TList;
+import com.julienvey.trello.impl.TrelloImpl;
+import com.julienvey.trello.impl.http.ApacheHttpClient;
+import edu.woodson.util.trello.TrelloForTwitter;
+import edu.woodson.util.trello.util.MovedCard;
+import edu.woodson.util.trello.util.TrelloList;
 import twitter4j.Paging;
 import twitter4j.Twitter;
+import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
 import twitter4j.conf.Configuration;
 import twitter4j.conf.ConfigurationBuilder;
 
 import java.io.PrintStream;
+import java.util.List;
 import java.util.Scanner;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class Twitter_Driver {
     public static void main(String[] args) {
@@ -61,6 +73,58 @@ public class Twitter_Driver {
 
             // PART IV
             //bigBird.investigate();
+            Timer timer = new Timer();
+
+            final String API_KEY = "6a888dffccb4c413711d7d617057fa07";
+            final String TOKEN = "186d9044cd5dfdb60c3b5ab3befb2aaeb2daddccdd05244ce152743701fec680";
+
+            System.out.println("Enter the Trello board id to listen to: ");
+            String boardId = scan.next();
+
+            Trello trello = new TrelloImpl(API_KEY, TOKEN, new ApacheHttpClient());
+
+
+            final Board[] board = new Board[1];
+            final TrelloList[] oldTrelloList = new TrelloList[1];
+
+            final int[] time = {0};
+            final boolean[] initial = {true};
+            TimerTask listenForMovedCards = new TimerTask() {
+                @Override
+                public void run() {
+                    if (initial[0]) {
+                        board[0] = trello.getBoard(boardId);
+                        List<TList> oldLists = board[0].fetchLists();
+                        oldTrelloList[0] = TrelloForTwitter.getTrelloList(trello, oldLists);
+                        initial[0] = false;
+                    }
+
+
+                    if (time[0] == 3) {
+                        initial[0] = true;
+
+                        Board newBoard = trello.getBoard(boardId);
+                        List<TList> newLists = newBoard.fetchLists();
+                        TrelloList newTrelloList = TrelloForTwitter.getTrelloList(trello, newLists);
+
+                        MovedCard moved = TrelloForTwitter.findMovedCard(board[0], oldTrelloList[0], newTrelloList);
+
+                        if (moved != null) {
+                            try {
+                                bigBird.tweetOut("You moved " + moved.getName() + " from " + moved.getFrom().getName() + " to " + moved.getTo().getName());
+                            } catch (TwitterException e) {
+                                e.printStackTrace();
+                            }
+                        }
+
+                        time[0] = 0;
+                    }
+                    time[0]++;
+                }
+            };
+
+            timer.scheduleAtFixedRate(listenForMovedCards, 0, 1000);
+
         } catch (Exception e) {
             e.printStackTrace();
         }
