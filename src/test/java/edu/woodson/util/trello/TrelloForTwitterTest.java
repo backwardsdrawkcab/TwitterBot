a9701ac617ae5a30f0b3af4d6fbdb475c9452756
JoshH <hilbertjosh1@gmail.com>
2019-03-13 22:18:45 -0400

    Remake TrelloTest

--- src/test/java/edu/woodson/util/trello/TrelloForTwitterTest.java
+++ src/test/java/edu/woodson/util/trello/TrelloForTwitterTest.java
@@ -1,29 +1,29 @@
-package edu.woodson;
+package edu.woodson.util.trello;
 
 import com.julienvey.trello.Trello;
 import com.julienvey.trello.domain.Board;
 import com.julienvey.trello.domain.TList;
 import com.julienvey.trello.impl.TrelloImpl;
 import com.julienvey.trello.impl.http.ApacheHttpClient;
-import edu.woodson.util.trello.TrelloForTwitter;
 import edu.woodson.util.trello.util.MovedCard;
 import edu.woodson.util.trello.util.TrelloList;
 
 import java.util.List;
 import java.util.Scanner;
 
-public class NewTrelloTest {
 
-    // Using trello account
-    // Email: twitterinterfacewithtrello@gmail.com
-    // Username: @twitterinterfacewithuser
-    // Password: wtwoodsontwitterlab2019twitterinterfacewithtrello
-
-
-    private static final String API_KEY = "6a888dffccb4c413711d7d617057fa07";
-    private static final String TOKEN = "186d9044cd5dfdb60c3b5ab3befb2aaeb2daddccdd05244ce152743701fec680";
+class TrelloForTwitterTest {
 
     public static void main(String[] args) {
+        // Using trello account
+        // Email: twitterinterfacewithtrello@gmail.com
+        // Username: @twitterinterfacewithuser
+        // Password: wtwoodsontwitterlab2019twitterinterfacewithtrello
+
+
+        final String API_KEY = "6a888dffccb4c413711d7d617057fa07";
+        final String TOKEN = "186d9044cd5dfdb60c3b5ab3befb2aaeb2daddccdd05244ce152743701fec680";
+
         Scanner sc = new Scanner(System.in);
 
         Trello trello = new TrelloImpl(API_KEY, TOKEN, new ApacheHttpClient());
@@ -33,7 +33,9 @@ public class NewTrelloTest {
 
         TrelloList oldTrelloList = TrelloForTwitter.getTrelloList(trello, oldLists);
 
+        System.out.println("Move the Test Card 6 (Moved) card in the Trello Test board from Doing to Done and then enter any key...");
         sc.next();
+
         Board newBoard = trello.getBoard(boardId);
         List<TList> newLists = newBoard.fetchLists();
 
@@ -41,9 +43,10 @@ public class NewTrelloTest {
 
         MovedCard moved = TrelloForTwitter.findMovedCard(board, oldTrelloList, newTrelloList);
 
-        assert moved != null;
-        System.out.println("You moved " + moved.getName() + " from " + moved.getFrom().getName() + " to " + moved.getTo().getName());
+        MovedCard expected = new MovedCard("Test Card 6 (Moved)", board.fetchLists().get(1), board.fetchLists().get(2));
 
+        System.out.println("Expected: " + expected.getName() + " moved from " + expected.getFrom().getName() + " to " + expected.getTo().getName());
+        System.out.println("Reality: " + moved.getName() + " moved from " + moved.getFrom().getName() + " to " + moved.getTo().getName());
     }
 
 }
